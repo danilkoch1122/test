07d423cf7c7de79ef3663da157159cb33c84539c
danilkoch1122 <github654@mail.ru>
2026-05-07 19:14:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gutlesson{
+class Program()
+{
+  public static void Main()
+    {
+      System.Console.WriteLine("sws");
+      System.Console.WriteLine("sss");      
+    }
+  }
+}
